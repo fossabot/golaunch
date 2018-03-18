53834364faed2302138b72f09caba9dd3aba61ad
micnncim <rerrkfuuul.85@gmail.com>
2018-03-18 21:06:00 +0900

    Fix bug: cannot launch app

--- cmd/launch.go
+++ cmd/launch.go
@@ -13,42 +13,33 @@ import (
 )
 
 func Launch(c *cli.Context) error {
-	dir := "/Applications"
-	names, err := util.GetLocalAppNames(dir)
+	dataDir := os.Getenv("HOME") + "/tmp"
+	items, err := util.ReadAppDataFiles(dataDir)
 	if err != nil {
-		fmt.Printf("GetLocalAppNames: %s\n", err)
-		return err
-	}
-
-	items, _, err := util.GetItems(names)
-	if err != nil {
-		fmt.Printf("GetItems: %s\n", err)
+		fmt.Println(err)
 		return err
 	}
 
 	itemRows, err := items.Render()
-	for _, itemRow := range itemRows {
-		fmt.Println(itemRow)
-	}
 	if err != nil {
-		fmt.Printf("Render: %s\n", err)
+		fmt.Println(err)
 		return err
 	}
 
 	command := "fzf"
 	rows, err := interactive.Select(command, itemRows)
 	if err != nil {
-		fmt.Printf("Select: %s\n", err)
+		fmt.Println(err)
 		return err
 	}
 
-	for _, row := range rows {
-		elements := strings.Split(row, "\t")
-		app := elements[0] + ".app"
-		cmd := exec.Command("open", "-a", app)
-		cmd.Stdin = os.Stdin
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
+	app := strings.Split(rows[0], "\t")[0]
+	cmd := exec.Command("open", "-a", app)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		return err
 	}
 
 	return nil
