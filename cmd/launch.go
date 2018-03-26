5216a27f7587c7fbc758333ff35495719b497726
micnncim <rerrkfuuul.85@gmail.com>
2018-03-26 20:40:43 +0900

    Install micnncim/picker

--- cmd/launch.go
+++ cmd/launch.go
@@ -2,14 +2,18 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"strings"
 
 	"github.com/mitchellh/go-homedir"
+	termbox "github.com/nsf/termbox-go"
+	"github.com/pkg/errors"
 	"github.com/skratchdot/open-golang/open"
 	"github.com/urfave/cli"
 
 	"github.com/micnncim/golaunch/util"
-	"github.com/micnncim/interactive"
+	"github.com/micnncim/picker"
 )
 
 func Launch(c *cli.Context) error {
@@ -32,16 +36,59 @@ func Launch(c *cli.Context) error {
 		return err
 	}
 
-	command := "fzf"
-	rows, err := interactive.Select(command, appDetailRows)
-	if err != nil {
-		fmt.Println(err)
-		return err
+	if err := termbox.Init(); err != nil {
+		return errors.Wrap(err, "failed to initialize termbox")
 	}
 
-	app := strings.Split(rows[0], "\t")[0]
-	if err := open.Run(app); err != nil {
-		return err
+	x, y := 0, 1
+	prompt := "QUERY>"
+	rows := picker.MakeRows(appDetailRows, termbox.ColorDefault, termbox.ColorDefault)
+	marker := termbox.ColorCyan
+	s := picker.NewScreen(rows, x, y, "", prompt, termbox.ColorDefault, termbox.ColorDefault, termbox.ColorBlack, termbox.ColorWhite, marker)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer termbox.Close()
+
+	for {
+		s.Clear()
+		s.DrawAll()
+
+		switch ev := termbox.PollEvent(); ev.Type {
+		case termbox.EventKey:
+			switch ev.Key {
+			case termbox.KeyEnter:
+				text := picker.GetRow(s.CursorY, s.BaseFg, s.BaseBg).Text
+				termbox.Close()
+				app := fmt.Sprintf("%s/%s.app", AppDir, strings.Split(text, " ")[0])
+				if err := open.Run(app); err != nil {
+					fmt.Println(err)
+					os.Exit(1)
+				}
+				os.Exit(0)
+			case termbox.KeyEsc, termbox.KeyCtrlD, termbox.KeyCtrlC:
+				termbox.Close()
+				os.Exit(0)
+			case termbox.KeyBackspace, termbox.KeyDelete:
+				s.DecreaseInput()
+			case termbox.KeySpace:
+				s.IncreaseInput(' ')
+			case termbox.KeyArrowUp, termbox.KeyCtrlK, termbox.KeyCtrlP:
+				s.MoveCursorUpper()
+			case termbox.KeyArrowDown, termbox.KeyCtrlJ, termbox.KeyCtrlN:
+				s.MoveCursorLower()
+			default:
+				if ev.Ch != 0 {
+					s.IncreaseInput(ev.Ch)
+				}
+			}
+		}
+
+		if err := termbox.Flush(); err != nil {
+			log.Println(err)
+			os.Exit(1)
+		}
 	}
 
 	return nil
